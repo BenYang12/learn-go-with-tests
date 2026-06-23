eabab5a837e5119afd24425fb34aee71b3a1892e
Benjamin Yang <benyang12@dhcp-9-16-42-187.atx-us.ibm.com>
2026-06-23 12:42:49 -0500

    Hello, World complete

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 
 		if got != want {
@@ -14,12 +14,25 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 
 		if got != want {
 			assertCorrectMessage(t, got, want)
 		}
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
 	})
 }
 
